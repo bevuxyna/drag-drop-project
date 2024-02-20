08b933c059d791fa47ca81f89e0705d8d22c39ea
Vlada Melnikova <wannhorn@gmail.com>
2024-02-20 22:43:32 +0300

    Adding Entry & Output Configuration

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import { Component } from './base-component.js';
-import { validate, Validator } from '../utils/validation.js';
-import { autoBind } from '../decorators/autobind.js';
-import { projectState } from '../state/project-state.js';
+import { Component } from './base-component';
+import { validate, Validator } from '../utils/validation';
+import { autoBind } from '../decorators/autobind';
+import { projectState } from '../state/project-state';
 
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
