32fd37904356e12ce36853d95b09be76fb4a88fa
Vlada Melnikova <wannhorn@gmail.com>
2024-02-20 21:45:15 +0300

    fix app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import { ProjectInput } from './components/project-input.js';
 import { ProjectList } from './components/project-list.js';
 
-namespace DragDropInterfaces {
-  new ProjectInput();
-  new ProjectList('active');
-  new ProjectList('finished');
-}
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
